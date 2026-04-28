6b4394a34e67d75e50301d6cdcb70f76bbaa97b9
maniemagz-dotcom <maniemagz@gmail.com>
2026-04-28 08:18:29 +0300

    Update and saved .js

--- Chapter-6-Javascript/06-PetrolCalculator/script.js
+++ Chapter-6-Javascript/06-PetrolCalculator/script.js
@@ -1,7 +1,12 @@
+// Function to calculate total cost of petrol based on user input for cost per litre and number of litres
 function calculate() {
+    // Get the values from the input fields and convert them to floating-point numbers
     var costPerLit = parseFloat(document.getElementById("cost").value);
     var litres = parseFloat(document.getElementById("number").value);
+
+    //To calculate the total cost by multiplying cost per litre with number of litres
     var total = costPerLit * litres;
     
-    document.getElementById("total").textContent = "Total cost: $" + total.toFixed(2);
+    // Displaying of result in <p> element
+    document.getElementById("total").textContent = "Total cost: $";
 }
